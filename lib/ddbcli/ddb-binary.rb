3054ae3931390853c5021c2bf8bab40f5a978b31
Genki Sugawara <sugawara@cookpad.com>
2013-05-13 16:49:46 +0900

    fix binary type (add base64 encode)

--- lib/ddbcli/ddb-binary.rb
+++ lib/ddbcli/ddb-binary.rb
@@ -1,12 +1,17 @@
+require 'base64'
+
 module DynamoDB
   class Binary
-    attr_reader :value
-    alias to_s value
-    alias to_str value
-
     def initialize(value)
       @value = value
     end
+
+    def value
+      Base64.encode64(@value).delete("\n")
+    end
+
+    alias to_s value
+    alias to_str value
 
     def inspect
       @value.inspect
