89dcbba9ffd6aae2c082eacee721722006fe1f86
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-12-14 14:05:20 +0900

    fix spec

--- spec/ddbcli_spec.rb
+++ spec/ddbcli_spec.rb
@@ -41,4 +41,114 @@ describe 'ddbcli' do
  "ItemCount"=>0}
     )
   end
+
+  it 'create table (hash and range)' do
+    ddbcli(<<-'EOS')
+      CREATE TABLE `foo` (
+        `id`  NUMBER HASH,
+        `val` STRING RANGE
+      ) read=2 write=2
+    EOS
+
+    out = ddbcli('desc foo')
+    out = JSON.parse(out)
+    out.delete('CreationDateTime')
+
+    expect(out).to eq(
+{"AttributeDefinitions"=>
+  [{"AttributeName"=>"id", "AttributeType"=>"N"},
+   {"AttributeName"=>"val", "AttributeType"=>"S"}],
+ "TableName"=>"foo",
+ "KeySchema"=>
+  [{"AttributeName"=>"id", "KeyType"=>"HASH"},
+   {"AttributeName"=>"val", "KeyType"=>"RANGE"}],
+ "TableStatus"=>"ACTIVE",
+ "ProvisionedThroughput"=>
+  {"NumberOfDecreasesToday"=>0,
+   "ReadCapacityUnits"=>2,
+   "WriteCapacityUnits"=>2},
+ "TableSizeBytes"=>0,
+ "ItemCount"=>0}
+    )
+  end
+
+  it 'create table with LSI' do
+    ddbcli(<<-'EOS')
+      CREATE TABLE `foo` (
+        `id`  NUMBER HASH,
+        `val` STRING RANGE,
+        INDEX `idx_bar` (`val2` STRING) ALL
+      ) read=2 write=2
+    EOS
+
+    out = ddbcli('desc foo')
+    out = JSON.parse(out)
+    out.delete('CreationDateTime')
+
+    expect(out).to eq(
+{"AttributeDefinitions"=>
+  [{"AttributeName"=>"id", "AttributeType"=>"N"},
+   {"AttributeName"=>"val", "AttributeType"=>"S"},
+   {"AttributeName"=>"val2", "AttributeType"=>"S"}],
+ "TableName"=>"foo",
+ "KeySchema"=>
+  [{"AttributeName"=>"id", "KeyType"=>"HASH"},
+   {"AttributeName"=>"val", "KeyType"=>"RANGE"}],
+ "TableStatus"=>"ACTIVE",
+ "ProvisionedThroughput"=>
+  {"NumberOfDecreasesToday"=>0,
+   "ReadCapacityUnits"=>2,
+   "WriteCapacityUnits"=>2},
+ "TableSizeBytes"=>0,
+ "ItemCount"=>0,
+ "LocalSecondaryIndexes"=>
+  [{"IndexName"=>"idx_bar",
+    "KeySchema"=>
+     [{"AttributeName"=>"id", "KeyType"=>"HASH"},
+      {"AttributeName"=>"val2", "KeyType"=>"RANGE"}],
+    "Projection"=>{"ProjectionType"=>"ALL"},
+    "IndexSizeBytes"=>0,
+    "ItemCount"=>0}]}
+    )
+  end
+
+  it 'create table with GSI' do
+    ddbcli(<<-'EOS')
+      CREATE TABLE `foo` (
+        `id`  NUMBER HASH,
+        `val` STRING RANGE,
+        GLOBAL INDEX `idx_bar` (`val2` STRING) ALL read=1 write=1
+      ) read=2 write=2
+    EOS
+
+    out = ddbcli('desc foo')
+    out = JSON.parse(out)
+    out.delete('CreationDateTime')
+
+    expect(out).to eq(
+{"AttributeDefinitions"=>
+  [{"AttributeName"=>"id", "AttributeType"=>"N"},
+   {"AttributeName"=>"val", "AttributeType"=>"S"},
+   {"AttributeName"=>"val2", "AttributeType"=>"S"}],
+ "TableName"=>"foo",
+ "KeySchema"=>
+  [{"AttributeName"=>"id", "KeyType"=>"HASH"},
+   {"AttributeName"=>"val", "KeyType"=>"RANGE"}],
+ "TableStatus"=>"ACTIVE",
+ "ProvisionedThroughput"=>
+  {"NumberOfDecreasesToday"=>0,
+   "ReadCapacityUnits"=>2,
+   "WriteCapacityUnits"=>2},
+ "TableSizeBytes"=>0,
+ "ItemCount"=>0,
+ "GlobalSecondaryIndexes"=>
+  [{"IndexName"=>"idx_bar",
+    "KeySchema"=>[{"AttributeName"=>"val2", "KeyType"=>"HASH"}],
+    "Projection"=>{"ProjectionType"=>"ALL"},
+    "IndexStatus"=>"ACTIVE",
+    "ProvisionedThroughput"=>{"ReadCapacityUnits"=>1, "WriteCapacityUnits"=>1},
+    "IndexSizeBytes"=>0,
+    "ItemCount"=>0}]}
+    )
+  end
 end
